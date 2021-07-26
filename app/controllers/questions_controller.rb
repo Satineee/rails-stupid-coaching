af4cf185a692fb828f6184f09d1fd240d25be83f
satine fouque <satine.fouque@gmail.com>
2021-07-26 15:04:22 +0400

    challenge reussi

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
-    puts 'Je suis dans Ask'
   end
 
   def answer
-    if params[:answer] == 'I am going to work'
-        @answer = 'Great'
-    elsif params[:answer]
-        @answer = 'Silly question, get dressed and go to work!'
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great !'
+    elsif @question.byteslice(-1) == '?'
+      @answer = 'Silly question, get dressed and go to work!'
     else
-        @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
